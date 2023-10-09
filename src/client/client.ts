9647db7cf60364890b4be259a010df168f18ee44
gyeongyeon <gjeon0321@gmail.com>
2023-10-10 03:31:42 +0900

    달위에 떨어지는 공

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,386 +1,142 @@
-import * as THREE from 'three'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'dat.gui'
-import * as CANNON from 'cannon-es'
-import CannonDebugRenderer from './utils/cannonDebugRenderer'
+// Import modules
+import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const scene = new THREE.Scene()
+// Create a scene
+const scene = new THREE.Scene();
 
-const light = new THREE.DirectionalLight()
-light.position.set(25, 50, 25)
-light.castShadow = true
-light.shadow.mapSize.width = 16384
-light.shadow.mapSize.height = 16384
-light.shadow.camera.near = 0.5
-light.shadow.camera.far = 100
-light.shadow.camera.top = 100
-light.shadow.camera.bottom = -100
-light.shadow.camera.left = -100
-light.shadow.camera.right = 100
-scene.add(light)
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 5, 10);
 
-const helper = new THREE.CameraHelper(light.shadow.camera)
-scene.add(helper)
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-const chaseCam = new THREE.Object3D()
-chaseCam.position.set(0, 0, 0)
-const chaseCamPivot = new THREE.Object3D()
-chaseCamPivot.position.set(0, 2, 4)
-chaseCam.add(chaseCamPivot)
-scene.add(chaseCam)
+// Create a light
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 10, 10);
+scene.add(light);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-document.body.appendChild(renderer.domElement)
+// Create a world
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0); // m/s²
 
-const phongMaterial = new THREE.MeshPhongMaterial()
+// Create a moon geometry and material (assuming the moon's radius is similar to the sphere)
+const moonGeometry = new THREE.SphereGeometry(3, 32, 32);
+const moonTexture = new THREE.TextureLoader().load("img/moon_map.jpg"); // 달 이미지 로드
+const moonMaterial = new THREE.MeshLambertMaterial({ map: moonTexture });
 
-const world = new CANNON.World()
-world.gravity.set(0, -9.82, 0)
+// Create a moon mesh and add it to the scene
+const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+scene.add(moonMesh);
 
-const groundMaterial = new CANNON.Material('groundMaterial')
-groundMaterial.friction = 0.25
-groundMaterial.restitution = 0.25
+// Create a moon shape and body
+const moonShape = new CANNON.Sphere(3); // radius (adjust it according to your moon size)
+const moonBody = new CANNON.Body({ mass: 0 }); // Set mass to 0 for a static moon
+moonBody.addShape(moonShape);
+moonBody.position.set(0, 0, 0); // m (adjust it according to the desired moon position)
+world.addBody(moonBody);
 
-const wheelMaterial = new CANNON.Material('wheelMaterial')
-wheelMaterial.friction = 0.25
-wheelMaterial.restitution = 0.25
+// Create a sphere geometry and material for the player's unit (red ball)
+const unitGeometry = new THREE.SphereGeometry(0.5, 16, 16);
+const unitMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 }); // Red color
 
-//ground
-const groundGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(100, 100)
-const groundMesh: THREE.Mesh = new THREE.Mesh(groundGeometry, phongMaterial)
-groundMesh.rotateX(-Math.PI / 2)
-groundMesh.receiveShadow = true
-scene.add(groundMesh)
-const groundShape = new CANNON.Box(new CANNON.Vec3(50, 1, 50))
-const groundBody = new CANNON.Body({ mass: 0, material: groundMaterial })
-groundBody.addShape(groundShape)
-groundBody.position.set(0, -1, 0)
-world.addBody(groundBody)
+// Create the player's unit (red ball) and add it to the scene
+const unitMesh = new THREE.Mesh(unitGeometry, unitMaterial);
+scene.add(unitMesh);
 
-//jumps
-for (let i = 0; i < 100; i++) {
-    const jump = new THREE.Mesh(
-        new THREE.CylinderGeometry(0, 1, 0.5, 5),
-        phongMaterial
-    )
-    jump.position.x = Math.random() * 100 - 50
-    jump.position.y = 0.5
-    jump.position.z = Math.random() * 100 - 50
-    scene.add(jump)
+// Create a unit shape and body
+const unitShape = new CANNON.Sphere(0.5); // radius
+const unitBody = new CANNON.Body({ mass: 1 }); // kg
+unitBody.addShape(unitShape);
+unitBody.position.set(0, 5, 0); // m (adjust it according to the starting position)
+world.addBody(unitBody);
 
-    const cylinderShape = new CANNON.Cylinder(0.01, 1, 0.5, 5)
-    const cylinderBody = new CANNON.Body({ mass: 0 })
-    cylinderBody.addShape(cylinderShape, new CANNON.Vec3())
-    cylinderBody.position.x = jump.position.x
-    cylinderBody.position.y = jump.position.y
-    cylinderBody.position.z = jump.position.z
-    world.addBody(cylinderBody)
-}
+// Create an orbit control for the camera
+const orbitControl = new OrbitControls(camera, renderer.domElement);
+orbitControl.target.copy(unitMesh.position); // make the camera look at the unit
 
-const carBodyGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 2)
-const carBodyMesh: THREE.Mesh = new THREE.Mesh(carBodyGeometry, phongMaterial)
-carBodyMesh.position.y = 3
-carBodyMesh.castShadow = true
-scene.add(carBodyMesh)
-carBodyMesh.add(chaseCam)
+// Define the time step for the physics simulation
+const fixedTimeStep = 1.0 / 60.0; // seconds
 
-const carBodyShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 1))
-const carBody = new CANNON.Body({ mass: 1 })
-carBody.addShape(carBodyShape)
-carBody.position.x = carBodyMesh.position.x
-carBody.position.y = carBodyMesh.position.y
-carBody.position.z = carBodyMesh.position.z
-world.addBody(carBody)
+// Define the keyboard input variables
+let upPressed = false;
+let downPressed = false;
+let leftPressed = false;
+let rightPressed = false;
 
-//front left wheel
-const wheelLFGeometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-    0.33,
-    0.33,
-    0.2
-)
-wheelLFGeometry.rotateZ(Math.PI / 2)
-const wheelLFMesh: THREE.Mesh = new THREE.Mesh(wheelLFGeometry, phongMaterial)
-wheelLFMesh.position.x = -1
-wheelLFMesh.position.y = 3
-wheelLFMesh.position.z = -1
-wheelLFMesh.castShadow = true
-scene.add(wheelLFMesh)
-const wheelLFShape = new CANNON.Sphere(0.33)
-const wheelLFBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelLFBody.addShape(wheelLFShape)
-wheelLFBody.position.x = wheelLFMesh.position.x
-wheelLFBody.position.y = wheelLFMesh.position.y
-wheelLFBody.position.z = wheelLFMesh.position.z
-world.addBody(wheelLFBody)
+// Add event listeners for the keyboard input
+window.addEventListener("keydown", (event) => {
+    switch (event.code) {
+        case "ArrowUp":
+            upPressed = true;
+            break;
+        case "ArrowDown":
+            downPressed = true;
+            break;
+        case "ArrowLeft":
+            leftPressed = true;
+            break;
+        case "ArrowRight":
+            rightPressed = true;
+            break;
+    }
+});
 
-//front right wheel
-const wheelRFGeometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-    0.33,
-    0.33,
-    0.2
-)
-wheelRFGeometry.rotateZ(Math.PI / 2)
-const wheelRFMesh: THREE.Mesh = new THREE.Mesh(wheelRFGeometry, phongMaterial)
-wheelRFMesh.position.y = 3
-wheelRFMesh.position.x = 1
-wheelRFMesh.position.z = -1
-wheelRFMesh.castShadow = true
-scene.add(wheelRFMesh)
-const wheelRFShape = new CANNON.Sphere(0.33)
-const wheelRFBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelRFBody.addShape(wheelRFShape)
-wheelRFBody.position.x = wheelRFMesh.position.x
-wheelRFBody.position.y = wheelRFMesh.position.y
-wheelRFBody.position.z = wheelRFMesh.position.z
-world.addBody(wheelRFBody)
+window.addEventListener("keyup", (event) => {
+    switch (event.code) {
+        case "ArrowUp":
+            upPressed = false;
+            break;
+        case "ArrowDown":
+            downPressed = false;
+            break;
+        case "ArrowLeft":
+            leftPressed = false;
+            break;
+        case "ArrowRight":
+            rightPressed = false;
+            break;
+    }
+});
 
-//back left wheel
-const wheelLBGeometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-    0.4,
-    0.4,
-    0.33
-)
-wheelLBGeometry.rotateZ(Math.PI / 2)
-const wheelLBMesh: THREE.Mesh = new THREE.Mesh(wheelLBGeometry, phongMaterial)
-wheelLBMesh.position.y = 3
-wheelLBMesh.position.x = -1
-wheelLBMesh.position.z = 1
-wheelLBMesh.castShadow = true
-scene.add(wheelLBMesh)
-const wheelLBShape = new CANNON.Sphere(0.4)
-const wheelLBBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelLBBody.addShape(wheelLBShape)
-wheelLBBody.position.x = wheelLBMesh.position.x
-wheelLBBody.position.y = wheelLBMesh.position.y
-wheelLBBody.position.z = wheelLBMesh.position.z
-world.addBody(wheelLBBody)
+const clock = new THREE.Clock();
 
-//back right wheel
-const wheelRBGeometry: THREE.CylinderGeometry = new THREE.CylinderGeometry(
-    0.4,
-    0.4,
-    0.33
-)
-wheelRBGeometry.rotateZ(Math.PI / 2)
-const wheelRBMesh: THREE.Mesh = new THREE.Mesh(wheelRBGeometry, phongMaterial)
-wheelRBMesh.position.y = 3
-wheelRBMesh.position.x = 1
-wheelRBMesh.position.z = 1
-wheelRBMesh.castShadow = true
-scene.add(wheelRBMesh)
-const wheelRBShape = new CANNON.Sphere(0.4)
-const wheelRBBody = new CANNON.Body({ mass: 1, material: wheelMaterial })
-wheelRBBody.addShape(wheelRBShape)
-wheelRBBody.position.x = wheelRBMesh.position.x
-wheelRBBody.position.y = wheelRBMesh.position.y
-wheelRBBody.position.z = wheelRBMesh.position.z
-world.addBody(wheelRBBody)
-
-const leftFrontAxis = new CANNON.Vec3(1, 0, 0)
-const rightFrontAxis = new CANNON.Vec3(1, 0, 0)
-const leftBackAxis = new CANNON.Vec3(1, 0, 0)
-const rightBackAxis = new CANNON.Vec3(1, 0, 0)
-
-const constraintLF = new CANNON.HingeConstraint(carBody, wheelLFBody, {
-    pivotA: new CANNON.Vec3(-1, -0.5, -1),
-    axisA: leftFrontAxis,
-    maxForce: 0.99,
-})
-world.addConstraint(constraintLF)
-const constraintRF = new CANNON.HingeConstraint(carBody, wheelRFBody, {
-    pivotA: new CANNON.Vec3(1, -0.5, -1),
-    axisA: rightFrontAxis,
-    maxForce: 0.99,
-})
-world.addConstraint(constraintRF)
-const constraintLB = new CANNON.HingeConstraint(carBody, wheelLBBody, {
-    pivotA: new CANNON.Vec3(-1, -0.5, 1),
-    axisA: leftBackAxis,
-    maxForce: 0.99,
-})
-world.addConstraint(constraintLB)
-const constraintRB = new CANNON.HingeConstraint(carBody, wheelRBBody, {
-    pivotA: new CANNON.Vec3(1, -0.5, 1),
-    axisA: rightBackAxis,
-    maxForce: 0.99,
-})
-world.addConstraint(constraintRB)
-
-//rear wheel drive
-constraintLB.enableMotor()
-constraintRB.enableMotor()
-
-const keyMap: { [id: string]: boolean } = {}
-const onDocumentKey = (e: KeyboardEvent) => {
-    keyMap[e.code] = e.type === 'keydown'
-}
-
-let forwardVelocity = 0
-let rightVelocity = 0
-
-document.addEventListener('keydown', onDocumentKey, false)
-document.addEventListener('keyup', onDocumentKey, false)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
-const stats = new Stats()
-document.body.appendChild(stats.dom)
-
-const gui = new GUI()
-const physicsFolder = gui.addFolder('Physics')
-physicsFolder.add(world.gravity, 'x', -10.0, 10.0, 0.1)
-physicsFolder.add(world.gravity, 'y', -10.0, 10.0, 0.1)
-physicsFolder.add(world.gravity, 'z', -10.0, 10.0, 0.1)
-physicsFolder.open()
-
-const clock = new THREE.Clock()
-let delta
-
-const cannonDebugRenderer = new CannonDebugRenderer(scene, world)
-
-const v = new THREE.Vector3()
-let thrusting = false
-
+// Define the animation loop function
 function animate() {
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
 
-    helper.update()
+    // Update the physics world
+    const delta = Math.min(clock.getDelta(), 0.1); // limit delta time to avoid instability
+    world.step(fixedTimeStep, delta);
 
-    delta = Math.min(clock.getDelta(), 0.1)
-    world.step(delta)
-
-    cannonDebugRenderer.update()
-
-    // Copy coordinates from Cannon to Three.js
-    carBodyMesh.position.set(
-        carBody.position.x,
-        carBody.position.y,
-        carBody.position.z
-    )
-    carBodyMesh.quaternion.set(
-        carBody.quaternion.x,
-        carBody.quaternion.y,
-        carBody.quaternion.z,
-        carBody.quaternion.w
-    )
-
-    wheelLFMesh.position.set(
-        wheelLFBody.position.x,
-        wheelLFBody.position.y,
-        wheelLFBody.position.z
-    )
-    wheelLFMesh.quaternion.set(
-        wheelLFBody.quaternion.x,
-        wheelLFBody.quaternion.y,
-        wheelLFBody.quaternion.z,
-        wheelLFBody.quaternion.w
-    )
-
-    wheelRFMesh.position.set(
-        wheelRFBody.position.x,
-        wheelRFBody.position.y,
-        wheelRFBody.position.z
-    )
-    wheelRFMesh.quaternion.set(
-        wheelRFBody.quaternion.x,
-        wheelRFBody.quaternion.y,
-        wheelRFBody.quaternion.z,
-        wheelRFBody.quaternion.w
-    )
-
-    wheelLBMesh.position.set(
-        wheelLBBody.position.x,
-        wheelLBBody.position.y,
-        wheelLBBody.position.z
-    )
-    wheelLBMesh.quaternion.set(
-        wheelLBBody.quaternion.x,
-        wheelLBBody.quaternion.y,
-        wheelLBBody.quaternion.z,
-        wheelLBBody.quaternion.w
-    )
-
-    wheelRBMesh.position.set(
-        wheelRBBody.position.x,
-        wheelRBBody.position.y,
-        wheelRBBody.position.z
-    )
-    wheelRBMesh.quaternion.set(
-        wheelRBBody.quaternion.x,
-        wheelRBBody.quaternion.y,
-        wheelRBBody.quaternion.z,
-        wheelRBBody.quaternion.w
-    )
-
-    thrusting = false
-    if (keyMap['KeyW'] || keyMap['ArrowUp']) {
-        if (forwardVelocity < 100.0) forwardVelocity += 1
-        thrusting = true
+    // Apply forces to the unit according to the keyboard input
+    const forceFactor = 10; // N (adjust it according to the desired force)
+    if (upPressed) {
+        unitBody.applyLocalForce(new CANNON.Vec3(0, 0, -forceFactor), new CANNON.Vec3(0, 0, 0));
     }
-    if (keyMap['KeyS'] || keyMap['ArrowDown']) {
-        if (forwardVelocity > -100.0) forwardVelocity -= 1
-        thrusting = true
+    if (downPressed) {
+        unitBody.applyLocalForce(new CANNON.Vec3(0, 0, forceFactor), new CANNON.Vec3(0, 0, 0));
     }
-    if (keyMap['KeyA'] || keyMap['ArrowLeft']) {
-        if (rightVelocity > -1.0) rightVelocity -= 0.1
+    if (leftPressed) {
+        unitBody.applyLocalForce(new CANNON.Vec3(-forceFactor, 0, 0), new CANNON.Vec3(0, 0, 0));
     }
-    if (keyMap['KeyD'] || keyMap['ArrowRight']) {
-        if (rightVelocity < 1.0) rightVelocity += 0.1
-    }
-    if (keyMap['Space']) {
-        if (forwardVelocity > 0) {
-            forwardVelocity -= 1
-        }
-        if (forwardVelocity < 0) {
-            forwardVelocity += 1
-        }
+    if (rightPressed) {
+        unitBody.applyLocalForce(new CANNON.Vec3(forceFactor, 0, 0), new CANNON.Vec3(0, 0, 0));
     }
 
-    if (!thrusting) {
-        //not going forward or backwards so gradually slow down
-        if (forwardVelocity > 0) {
-            forwardVelocity -= 0.25
-        }
-        if (forwardVelocity < 0) {
-            forwardVelocity += 0.25
-        }
-    }
+    // Copy the position from the unit body to the unit mesh
+    unitMesh.position.copy(new THREE.Vector3(unitBody.position.x, unitBody.position.y, unitBody.position.z));
 
-    constraintLB.setMotorSpeed(forwardVelocity)
-    constraintRB.setMotorSpeed(forwardVelocity)
-    constraintLF.axisA.z = rightVelocity
-    constraintRF.axisA.z = rightVelocity
+    // Update the orbit control
+    orbitControl.update();
 
-    camera.lookAt(carBodyMesh.position)
-
-    chaseCamPivot.getWorldPosition(v)
-    if (v.y < 1) {
-        v.y = 1
-    }
-    camera.position.lerpVectors(camera.position, v, 0.05)
-
-    render()
-
-    stats.update()
+    // Render the scene
+    renderer.render(scene, camera);
 }
 
-function render() {
-    renderer.render(scene, camera)
-}
-
-animate()
+// Start the animation loop
+animate();
